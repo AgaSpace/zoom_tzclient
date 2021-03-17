e85d7374d32afc7c14774cb7228315f99d70696b
shell627 <n1eversky@gmail.com>
2021-03-17 15:00:15 +0600

    debugger update v3

--- TerraZLauncher/TZLauncher/TerraZLauncherDebugger/Program.cs
+++ TerraZLauncher/TZLauncher/TerraZLauncherDebugger/Program.cs
@@ -12,44 +12,12 @@ namespace TerraZLauncherDebugger
     {
         static void Main(string[] args)
         {
-            LauncherCore.WriteInfoBG("/pause >>> LAUNCHER::PAUSE");
-            LauncherCore.WriteInfoBG("/resume >>> LAUNCHER::RESUME");
-            LauncherCore.WriteInfoBG("/stop >>> LAUNCHER::STOP");
-            LauncherCore.WriteInfoBG("/push >>> LAUNCHER::PUSH");
             Thread t = new Thread(() =>
             {
                 Assembly asm = Assembly.LoadFrom("TZLauncher.exe");
                 asm.Launch(new string[0] { });
             });
             t.Start();
-
-            for (;;)
-            {
-                switch (Console.ReadLine())
-                {
-                    case "/pause":
-                        LauncherCore.WriteInfoBG("LAUNCHER::PAUSE");
-                        t.Suspend();
-                        break;
-                    case "/resume":
-                        LauncherCore.WriteInfoBG("LAUNCHER::RESUME");
-                        t.Resume();
-                        break;
-                    case "/stop":
-                        LauncherCore.WriteInfoBG("LAUNCHER::STOP");
-                        t.Abort();
-                        break;
-                    case "/push":
-                        LauncherCore.WriteInfoBG("LAUNCHER::PUSH");
-                        t = new Thread(() =>
-                        {
-                            Assembly asm = Assembly.LoadFrom("TZLauncher.exe");
-                            asm.Launch(new string[0] { });
-                        });
-                        t.Start();
-                        break;
-                }
-            }
         }
     }
     public static class LauncherCore
